94cb904a43d43ce628a3f59a43935040d6cf195b
Marco Boscardin <marcoboscardin94@gmail.com>
2018-03-26 04:26:59 +0200

    User profile image persistent. Improvements in graphics, font, textView/scrollView. In edit profile current image added. Some bugs fixed.
    TODO:
    - manage different screen sizes
    - improve interface appearance (color, text, constraint, etc...)
    - landscape view for show profile

--- app/src/main/java/com/example/group6/appsharebook/EditProfile.java
+++ app/src/main/java/com/example/group6/appsharebook/EditProfile.java
@@ -1,42 +1,36 @@
 package com.example.group6.appsharebook;
 
-import android.Manifest;
+import android.content.Context;
+import android.content.ContextWrapper;
 import android.content.DialogInterface;
 import android.content.Intent;
 import android.content.pm.PackageManager;
 import android.graphics.Bitmap;
-import android.media.MediaScannerConnection;
+import android.graphics.BitmapFactory;
 import android.net.Uri;
-import android.os.Environment;
 import android.provider.MediaStore;
 import android.support.v4.app.ActivityCompat;
 import android.support.v4.content.ContextCompat;
 import android.support.v7.app.AlertDialog;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
-import android.util.Log;
 import android.view.View;
 import android.widget.EditText;
 import android.widget.ImageView;
 import android.widget.Toast;
-
-import java.io.ByteArrayOutputStream;
 import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
 import java.io.FileOutputStream;
 import java.io.IOException;
-import java.util.Calendar;
-
 import static android.Manifest.permission.CAMERA;
 import static android.Manifest.permission.READ_EXTERNAL_STORAGE;
 import static android.Manifest.permission.WRITE_EXTERNAL_STORAGE;
 
 public class EditProfile extends AppCompatActivity {
     private int GALLERY = 1, CAMERA_S = 2;
-    private static final String IMAGE_DIRECTORY = "/group6";
-    ImageView image23,image1;
+    ImageView image1;
     EditText name1,surname1,email1,userBio1;
-    private static final int PICK_IMAGE = 100;
-    Uri imageUri;
 
     public static final int RequestPermissionCode = 7;
     @Override
@@ -49,7 +43,7 @@ public class EditProfile extends AppCompatActivity {
         email1 = findViewById(R.id.userEmailEditText);
         userBio1 = findViewById(R.id.userBioEditText);
 
-        image23 = findViewById(R.id.imageView);
+        loadImageFromStorage();
 
         image1.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -62,10 +56,8 @@ public class EditProfile extends AppCompatActivity {
 
     public void sendData (View v) {
 
-
         Intent returnIntent = new Intent();
-        Bitmap image = image1.getDrawingCache();
-        returnIntent.putExtra("image",image);
+
         String name = name1.getText().toString();
         returnIntent.putExtra("name",name);
         String surname = surname1.getText().toString();
@@ -75,11 +67,8 @@ public class EditProfile extends AppCompatActivity {
         String userBio = userBio1.getText().toString();
         returnIntent.putExtra("userBio",userBio);
 
-
         setResult(RESULT_OK, returnIntent);
         finish();
-
-
     }
 
     private void openGallery(){
@@ -87,7 +76,6 @@ public class EditProfile extends AppCompatActivity {
         checkPer();
 
         AlertDialog.Builder pictureDialog = new AlertDialog.Builder(this);
-        //pictureDialog.setTitle("Select Action");
         pictureDialog.setTitle(getResources().getString(R.string.select_action));
         String[] pictureDialogItems = {
                 getResources().getString(R.string.select_photo_gallery),
@@ -118,9 +106,9 @@ public class EditProfile extends AppCompatActivity {
                 Uri contentURI = data.getData();
                 try {
                     Bitmap bitmap = MediaStore.Images.Media.getBitmap(this.getContentResolver(), contentURI);
-                    String path = saveImage(bitmap);
+                    saveToInternalStorage(bitmap);
                     Toast.makeText(EditProfile.this, getResources().getString(R.string.image_saved), Toast.LENGTH_SHORT).show();
-                    image1.setImageBitmap(bitmap);
+                    image1.setImageBitmap(bitmap); //show the image into the interface
 
                 } catch (IOException e) {
                     e.printStackTrace();
@@ -131,7 +119,7 @@ public class EditProfile extends AppCompatActivity {
         } else if (requestCode == CAMERA_S) {
             Bitmap thumbnail = (Bitmap) data.getExtras().get("data");
             image1.setImageBitmap(thumbnail);
-            saveImage(thumbnail);
+            saveToInternalStorage(thumbnail);
             Toast.makeText(EditProfile.this, getResources().getString(R.string.image_saved), Toast.LENGTH_SHORT).show();
         }
     }
@@ -147,42 +135,38 @@ public class EditProfile extends AppCompatActivity {
         startActivityForResult(intent, CAMERA_S);
     }
 
-    public String saveImage(Bitmap myBitmap) {
-        ByteArrayOutputStream bytes = new ByteArrayOutputStream();
-        myBitmap.compress(Bitmap.CompressFormat.JPEG, 90, bytes);
-        File wallpaperDirectory = new File(
-                Environment.getExternalStorageDirectory() + IMAGE_DIRECTORY);
-        // have the object build the directory structure, if needed.
-        if (!wallpaperDirectory.exists()) {
-            wallpaperDirectory.mkdirs();
-        }
+    private String saveToInternalStorage(Bitmap bitmapImage){
 
+        //create the path where save the image
+        ContextWrapper cw = new ContextWrapper(getApplicationContext());
+        File directory = cw.getDir("userProfileImageDir", Context.MODE_PRIVATE); // Dir: /data/user/0/com.example.group6.appsharedbook/app_userProfileImageDir where user profile image is saved
+        File userProfileImagePath = new File(directory,"userProfileImage.jpg");
+
+        //save the image
+        FileOutputStream fos = null;
         try {
-            File f = new File(wallpaperDirectory, Calendar.getInstance()
-                    .getTimeInMillis() + ".jpg");
-            f.createNewFile();
-            FileOutputStream fo = new FileOutputStream(f);
-            fo.write(bytes.toByteArray());
-            MediaScannerConnection.scanFile(this,
-                    new String[]{f.getPath()},
-                    new String[]{"image/jpeg"}, null);
-            fo.close();
-            Log.d("TAG", getResources().getString(R.string.file_save) + f.getAbsolutePath());
-
-            return f.getAbsolutePath();
-        } catch (IOException e1) {
-            e1.printStackTrace();
+            fos = new FileOutputStream(userProfileImagePath);
+            bitmapImage.compress(Bitmap.CompressFormat.PNG, 100, fos); // Use the compress method on the BitMap object to write image to the OutputStream
         }
-        return "";
+        catch (Exception e) {
+            e.printStackTrace();
+        }
+        finally {
+            try {
+                fos.close();
+            }
+            catch (IOException e) {
+                e.printStackTrace();
+            }
+        }
+        return directory.getAbsolutePath();
     }
 
     private void checkPer(){
-        if(CheckingPermissionIsEnabledOrNot())
-        {
-            Toast.makeText(EditProfile.this, getResources().getString(R.string.all_permission_granted), Toast.LENGTH_LONG).show();
-        }
-        // If, If permission is not enabled then else condition will execute.
-        else {
+
+        //If permission is not enabled
+        if(!CheckingPermissionIsEnabledOrNot()) {
+
             //Calling method to enable permission.
             RequestMultiplePermission();
         }
@@ -234,4 +218,30 @@ public class EditProfile extends AppCompatActivity {
                 ThirdPermissionResult == PackageManager.PERMISSION_GRANTED;
     }
 
+    private void loadImageFromStorage(){
+
+        //Retrieve the user profile image
+        ContextWrapper cw = new ContextWrapper(getApplicationContext());
+        File directory = cw.getDir("userProfileImageDir", Context.MODE_PRIVATE); // Dir: /data/user/0/com.example.group6.appsharebook/app_userProfileImageDir where user profile image is saved
+        File userProfileImagePath = new File(directory,"userProfileImage.jpg");
+
+        if(userProfileImagePath.exists()) {
+
+            FileInputStream fis = null;
+            try {
+                fis = new FileInputStream(userProfileImagePath);
+                Bitmap b = BitmapFactory.decodeStream(fis);
+                image1 = findViewById(R.id.imageView);
+                image1.setImageBitmap(b);
+            } catch (FileNotFoundException e) {
+                e.printStackTrace();
+            } finally {
+                try {
+                    fis.close();
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
+            }
+        }
+    }
 }
--- app/src/main/java/com/example/group6/appsharebook/ShowProfile.java
+++ app/src/main/java/com/example/group6/appsharebook/ShowProfile.java
@@ -1,21 +1,29 @@
 package com.example.group6.appsharebook;
 
+import android.content.Context;
+import android.content.ContextWrapper;
 import android.content.Intent;
 import android.content.SharedPreferences;
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
 import android.graphics.Color;
-import android.media.Image;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.widget.ImageView;
 import android.widget.TextView;
+
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.IOException;
 
 public class ShowProfile extends AppCompatActivity {
     android.support.v7.widget.Toolbar tb;
-    TextView nameview,surnameView,emailView,bioView;
+    TextView nameView,surnameView,emailView,bioView;
     String name,surname,email,bio;
-
-    Image image1;
+    ImageView imageView;
 
     private static final int SECOND_ACTIVITY_REQUEST_CODE = 0;
     @Override
@@ -25,21 +33,23 @@ public class ShowProfile extends AppCompatActivity {
         tb = findViewById(R.id.toolbar);
         tb.setTitleTextColor(Color.WHITE);
         setSupportActionBar(tb);
-        nameview = findViewById(R.id.textViewUserName);
+
+        nameView = findViewById(R.id.textViewUserName);
         surnameView = findViewById(R.id.textViewUserSurname);
         emailView = findViewById(R.id.textViewUserEmail);
         bioView = findViewById(R.id.textViewUserBio);
 
-
         SharedPreferences settings = getPreferences(MODE_PRIVATE);
         String name1 = settings.getString("name", name);
-        nameview.setText(name1);
+        nameView.setText(name1);
         String surname1 = settings.getString("surname",surname);
         surnameView.setText(surname1);
         String email1 = settings.getString("email",email);
         emailView.setText(email1);
         String bio1 = settings.getString("userBio",bio);
         bioView.setText(bio1);
+
+        loadImageFromStorage();
     }
 
     public boolean onCreateOptionsMenu(Menu menu){
@@ -68,15 +78,19 @@ public class ShowProfile extends AppCompatActivity {
             if (resultCode == RESULT_OK) {
 
                 String name = data.getStringExtra("name");
-                nameview.setText(name);
+                if(!name.isEmpty())
+                    nameView.setText(name);
                 String surname = data.getStringExtra("surname");
-                surnameView.setText(surname);
+                if(!surname.isEmpty())
+                    surnameView.setText(surname);
                 String email = data.getStringExtra("email");
-                emailView.setText(email);
+                if(!email.isEmpty())
+                    emailView.setText(email);
                 String bio = data.getStringExtra("userBio");
-                bioView.setText(bio);
+                if(!bio.isEmpty())
+                    bioView.setText(bio);
 
-
+                loadImageFromStorage();
             }
         }
     }
@@ -84,13 +98,40 @@ public class ShowProfile extends AppCompatActivity {
     @Override
     protected void onPause() {
         super.onPause();
-        SharedPreferences settings = getPreferences(
-                MODE_PRIVATE);
+
+        SharedPreferences settings = getPreferences(MODE_PRIVATE);
         SharedPreferences.Editor editor = settings.edit();
-        editor.putString("name",nameview.getText().toString());
+        editor.putString("name",nameView.getText().toString());
         editor.putString("surname",surnameView.getText().toString());
         editor.putString("email",emailView.getText().toString());
         editor.putString("userBio",bioView.getText().toString());
         editor.commit();
+    }
+
+    private void loadImageFromStorage(){
+
+        //Retrieve the user profile image
+        ContextWrapper cw = new ContextWrapper(getApplicationContext());
+        File directory = cw.getDir("userProfileImageDir", Context.MODE_PRIVATE); // Dir: /data/user/0/com.example.group6.appsharebook/app_userProfileImageDir where user profile image is saved
+        File userProfileImagePath = new File(directory,"userProfileImage.jpg");
+
+        if(userProfileImagePath.exists()) {
+
+            FileInputStream fis = null;
+            try {
+                fis = new FileInputStream(userProfileImagePath);
+                Bitmap b = BitmapFactory.decodeStream(fis);
+                imageView = findViewById(R.id.imageView);
+                imageView.setImageBitmap(b);
+            } catch (FileNotFoundException e) {
+                e.printStackTrace();
+            } finally {
+                try {
+                    fis.close();
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
+            }
+        }
     }
 }
