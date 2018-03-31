1307244eb4752ce7dea7a3f4a1dba801f2d333a8
Marco Boscardin <marcoboscardin94@gmail.com>
2018-03-31 04:33:51 +0200

    -Circle profile image -new launcher icon -added new colors -changed text, window background and button color in styles.xml

--- app/src/main/java/com/example/group6/appsharebook/EditProfile.java
+++ app/src/main/java/com/example/group6/appsharebook/EditProfile.java
@@ -252,21 +252,31 @@ public class EditProfile extends AppCompatActivity {
     }
 
     private static Bitmap getRoundedCornerBitmap(Bitmap bitmap) {
-        Bitmap output = Bitmap.createBitmap(bitmap.getWidth(),
-                bitmap.getHeight(), Bitmap.Config.ARGB_8888);
-        Canvas canvas = new Canvas(output);
 
+        Bitmap output = Bitmap.createBitmap(bitmap.getWidth(), bitmap.getHeight(), Bitmap.Config.ARGB_8888);
+        Canvas canvas = new Canvas(output);
         final int color = 0xff424242;
         final Paint paint = new Paint();
         final Rect rect = new Rect(0, 0, bitmap.getWidth(), bitmap.getHeight());
-        final RectF rectF = new RectF(rect);
-        final float roundPx = 12;
 
         paint.setAntiAlias(true);
         canvas.drawARGB(0, 0, 0, 0);
         paint.setColor(color);
-        canvas.drawRoundRect(rectF, roundPx, roundPx, paint);
 
+        float cx = 0, cy = 0, r = 0;
+
+        if (bitmap.getWidth() > bitmap.getHeight()) {
+            cx = bitmap.getWidth() / 2;
+            cy = bitmap.getHeight() / 2;
+            r = bitmap.getHeight() / 2;
+        }
+        else {
+            cx = bitmap.getHeight() / 2;
+            cy = bitmap.getWidth() / 2;
+            r = bitmap.getWidth() / 2;
+        }
+
+        canvas.drawCircle(cx, cy, r, paint); //cx: x-coordinate of the center of the circle, cy: y-coordinate of the center, r: radius of the circle, paint: paint used to draw the circle
         paint.setXfermode(new PorterDuffXfermode(PorterDuff.Mode.SRC_IN));
         canvas.drawBitmap(bitmap, rect, rect, paint);
 
